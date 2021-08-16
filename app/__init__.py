02cea833d6f4369efbd38394cc7aa7d13b1b7e6f
David-V-Rullo <d.v.rullo@gmail.com>
2021-08-16 12:41:18 -0400

    Back to work on this

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from app.routes import home, dashboard
+from app.db import init_db
+
 def create_app(test_config=None):
     app = Flask(__name__, static_url_path='/')
     app.url_map.strict_slashes = False
@@ -7,11 +9,9 @@ def create_app(test_config=None):
         SECRET_KEY='super_secret_key'
     )
 
-    @app.route('/hello')
-    def hello():
-        return 'hello world'
-
     app.register_blueprint(home)
     app.register_blueprint(dashboard)
+    
+    init_db()
     
     return app
--- app/models/Post.py
+++ app/models/Post.py
@@ -1,10 +1,9 @@
 from datetime import datetime
+from .Vote import Vote
 from app.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
+from sqlalchemy.orm import relationship, column_property
 
-user = relationship('User')
-comments = relationship('Comment', cascade='all,delete')
 class Post(Base):
   __tablename__ = 'posts'
   id = Column(Integer, primary_key=True)
@@ -13,3 +12,9 @@ class Post(Base):
   user_id = Column(Integer, ForeignKey('users.id'))
   created_at = Column(DateTime, default=datetime.now)
   updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+  vote_count = column_property(select([func.count(Vote.id)]).where(Vote.post_id == id)
+)
+
+user = relationship('User')
+comments = relationship('Comment', cascade='all,delete')
+votes = relationship('Vote', cascade='all,delete')
